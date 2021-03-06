968fa333f3b8bd0f84b73cf2a91ff8e585581dd6
p870613 <p870613@yahoo.com.tw>
2021-03-06 17:03:45 +0800

    dp

---
+++ leetcode/5_Longest_Palindromic_Substring/sol_dp/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+
+bool check(bool **dp, int i, int j, int len){
+    if(i == j)
+        return true;
+    if((i + 1) == j)
+        return true;
+    
+    while(i < j){
+        if(i >= 0 && i < len && j >= 0 && j < len){
+            if(dp[i][j] == 1){
+                i ++;
+                j --;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+void print(bool **dp, int len){
+    for(int i = 0; i < len; i ++){
+        for(int j = 0; j < len; j++){
+            std::cout << dp[i][j] << " "; 
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "==========" << std::endl;
+}
+
+std::string longestPalindrome(std::string s) {
+    if(s.size() < 1)
+        return "";
+
+    const int len = s.size();
+    bool **dp = new bool*[len];
+    for(int i = 0; i < len; i++)
+        dp[i] = new bool(0);
+   
+    int start = 0, ans_len = 0; 
+    for(int i = 0; i < len; i++){
+        for(int index = 0; index < len && (index+i) < len; index ++){
+            if(s[index] == s[index+i] && check(dp, index+1, index+i-1, len)){
+                dp[index][index+i] = 1;
+                start = index;
+                ans_len = i;
+            }
+        }
+        //std::cout << i << std::endl;
+        //print(dp, len);
+    }
+    return s.substr(start, ans_len+1);
+}
+
+int main(){
+    std::string input;
+    std::cin >> input;
+    std::cout << longestPalindrome(input) << std::endl;; 
+}
