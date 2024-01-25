ce186416664a4adba5bc53d8e1faa76d4dd590cd
p870613 <p870613@yahoo.com.tw>
2024-01-25 22:38:34 +0800

    Create greedy.c

---
+++ leetcode/55. Jump Game/greedy.c
@@ -0,0 +1,14 @@
+bool canJump(int* nums, int numsSize) {
+    int max = 0;
+    for(int i = 0; i < numsSize; i++) {
+        if(max < i)
+            return false;
+
+        if(i + nums[i] > max)
+            max = i + nums[i];
+        
+        if(max >= numsSize - 1)
+            return true;
+    }
+    return false;
+}
