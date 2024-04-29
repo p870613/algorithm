aee5bb7e4c3e95aedeb4bda968b68b4b45eb9b48
p870613 <p870613@yahoo.com.tw>
2024-04-29 08:09:49 +0800

    Create main.cpp

---
+++ neetcode/Binary Search/704. Binary Search/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int m;
+
+        while (r >= l) {
+            m = (r + l) / 2;
+
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return -1;
+    }
+};
