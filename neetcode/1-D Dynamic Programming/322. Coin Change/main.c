30624af36d2cbd64f8e2572cb46bd9fec3ee9f58
p870613 <p870613@yahoo.com.tw>
2025-02-14 00:04:41 +0800

    Create main.c

---
+++ neetcode/1-D Dynamic Programming/322. Coin Change/main.c
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int dp[10001] = {100};
+
+        fill_n(dp, 10001, INT_MAX-1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
+                }
+                    
+            }
+        };
+        return dp[amount] == INT_MAX-1 ? -1 : dp[amount];
+    }
+};
